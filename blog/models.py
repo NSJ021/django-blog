025298a2401b757ab467ed960453e17513fecfa3
Nathan Jones <nsjones021@gmail.com>
2024-10-30 12:24:22 +0000

    Add: Ordering and __str__

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 
 STATUS =((0, "Draft"), (1, "Published"))
 
+
+# Post Model
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -17,7 +19,15 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    # Meta class and assigning string names to Post objects
+    class Meta:
+        ordering = ["-created_on"]
 
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+
+# Comment Model
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
@@ -26,3 +36,10 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    # Meta class and assigning string names to Post objects
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.body} by {self.author}"
