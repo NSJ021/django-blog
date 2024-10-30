cb480fad95fcf9aa9b211a69314171e6853572a7
Nathan Jones <nsjones021@gmail.com>
2024-10-30 09:46:23 +0000

    Debug edit

--- nj_blog/settings.py
+++ nj_blog/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-nsj021-djangoblog-mb18z1oci7a.ws.codeinstitute-ide.net', 
                 '.herokuapp.com']
