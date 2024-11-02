abdcfc9146707262fd0fe13bd78a0689ae973da9
Nathan Jones <104011128+NSJ021@users.noreply.github.com>
2024-11-02 20:49:34 +0000

    Delete env.py

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgres://u1tkfg53y0z:UWawLwUbGoXy@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/scowl_curse_risk_276445")
-
-os.environ.setdefault("SECRET_KEY", "django-insecure-pk5rxt$-qrn*2sc1eu+)jbp6xt9j(@^+jd9zf^fh5uqryaq9d)")
-
-os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary://411276469234688:h0jZW6vQXlcxtuzkHCFwjvOIvXA@dt5yygt6j")
